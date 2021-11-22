17b8496596eb1996460c18e46a8d1ef96b95c6e4
PierroD <frozenphenix78@gmail.com>
2021-11-22 22:46:02 +0100

    feat : add login page and wallpapers

--- index.php
+++ index.php
@@ -6,6 +6,7 @@ require "vendor/autoload.php";
 $router = new App\Routers\Router($_GET['url']);
 
 $router->get('/', function(){ echo 'test'; });
+$router->get('/login', "weather#showLogin");
 $router->get('/dashboard', "weather#show");
 $router->get('/dashboard/:name', "weather#showWithCityName");
 
--- src/app/Controllers/weatherController.php
+++ src/app/Controllers/weatherController.php
@@ -5,14 +5,14 @@ namespace App\Controllers;
 class weatherController {
 
     public function show() {
-        echo Controller::getTwig()->render('home.html.twig', ['person' => [
+        echo Controller::getTwig()->render('dashboard.html.twig', ['person' => [
             'name' => 'Paris'
         ]]);
     }
 
     public function showWithCityName($name) {
         
-        echo Controller::getTwig()->render('home.html.twig', ['person' => [
+        echo Controller::getTwig()->render('dashboard.html.twig', ['person' => [
             'name' => $name
         ]]);
     }
